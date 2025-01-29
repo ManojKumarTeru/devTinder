515c3fa35f3b00db60c256792b9ee9796b0fa859
Manoj Kumar Teru <manojteru963@gmail.com>
2025-01-30 02:17:04 +0530

    error Handling

--- src/app.js
+++ src/app.js
@@ -14,6 +14,12 @@ app.use("/login/pic",(req,res)=>{
     res.send("Iam your picture");
 })
 
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Contact Mr.Manoj Kumar!");
+    }
+})
+
 
 
 
