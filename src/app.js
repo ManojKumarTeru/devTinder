6230337e3038c6759d82b13cd298e22e51b13901
Manoj Kumar Teru <manojteru963@gmail.com>
2025-02-13 02:09:13 +0530

    Added signup api

--- src/app.js
+++ src/app.js
@@ -5,26 +5,38 @@ const app=express();
 
 const User=require("./models/user");
 
+app.use(express.json()); //it is a middleware to convert the user data into json (readable)
 
 app.post("/signup",async(req,res)=>{
-    const user=new User({
-        firstName:"Rohit",
-        lastName:"Sharma",
-        emailid:"rohit@sharma.cpm",
-        password:"roht@123"
-    })
+
+    // console.log(req.body);
+
+    const user=new User(req.body);
 
     try{
         await user.save();
-        res.send("User Added Successfully!");
-    } catch(err){
-        res.status(400).send("Error Saving the user:"+err.message);
+        res.send("User added successfully!");
+    }
+    catch(err){
+        res.status(400).send("Error saving the user data"+err.message);
     }
 
+
+    // const user=new User({
+    //     firstName:"Rohit",
+    //     lastName:"Sharma",
+    //     emailid:"rohit@sharma.cpm",
+    //     password:"roht@123"
+    // })
+
+    // try{
+    //     await user.save();
+    //     res.send("User Added Successfully!");
+    // } catch(err){
+    //     res.status(400).send("Error Saving the user:"+err.message);
+    // }
     
 })
-
-
 
 
 connectDB()
